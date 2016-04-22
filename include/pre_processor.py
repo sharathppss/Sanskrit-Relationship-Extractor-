d985ce141113b00982183cbab41cfb44d846fafa
suhasvprasad <suhasvprasad@gmail.com>
2016-04-21 20:16:51 -0700

    Commiting changes after code refactoring.

--- include/pre_processor.py
+++ include/pre_processor.py
@@ -3,65 +3,93 @@ from include import root_extractor as rx
 #from sets import Set
 import string
 
-def find_list(dic): #res=>Dict with element key and main word as value (reversing the main->form1,form2,form3 etc.., for synonoyms)
-    res={}
-    root_keys=dic.keys()
-    for ele in root_keys:
-        name_keys=dic[ele]
-        for n in name_keys:
-            res[n]=ele
-    return res
+def reverse_dictionary(dic): 
+    """Function to reverse a dictionary.
 
-def find_relation_tuples(str,names_all,name_rel, data_type): #names_all=> Dict with names key and main word as value (addressing synonoymns)
-    # #name_rel=> Dict with "n1|n2" keys and relationship value.
-    str=str.translate(string.maketrans(",:","  ")).strip() # strip punctuations and replace with space
-    tokens=str.split()
-    names=set()
-    res=[]
-    rel=set()
-    #print(names_all, name_rel)
-    #for name in names_all:
-    #    print(name, [ord(c) for c in name], bytes(name, 'utf-8'))
-    #names_all=find_list(name_dic)
-    #rel_all=find_list(rel_dic)
-    for tok in tokens:
-        #root=rx.root_ext(tok)
-        root=tok
-        #print(tok, [ord(c) for c in root], bytes(root, 'utf-8'))
-        h=names_all.keys()
-        if root in h:
-            names.add(names_all[root])
-    #print(names)
-    c=[]
-    for n1 in names:
-        c.append(n1)
-        for n2 in [x for x in names if x not in c]:
+    Parameters
+    ----------
+    dic : An input dictionary of the format {key: [value_1, value_2, ...], ...}
+
+    Returns
+    -------
+    result : Dictionary with the format {value_1: key, value_2: key, ...}
+    """ 
+    
+    result = {}
+    root_keys = dic.keys()
+    for element in root_keys:
+        name_keys = dic[element]
+        for name in name_keys:
+            result[name]=element
+    return result
+
+def find_relation_tuples(shloka, names_dict, relation_dict, data_type): 
+    """Function that extracts the features(words in the shloka) for a name pair 
+    if they are related. 
+
+    Parameters
+    ----------
+    shloka : A segment of Sanskrit text.
+    names_dict : A dictionary that contains synonym as key and the 
+                corresponding name as value.
+    relation_dict: A dictionary that contains 'name_1|name_2' as key and the 
+                    relation between name_1 and name_2 as value.
+    data_type : A string that specifies whether data is of type 'train' or 
+                'test'.
+
+    Returns
+    -------
+    result : A list of the format [name pair, relationship, features] if 
+            data_type is 'train'.
+            A lsit of the format [name_pair, features] if data_type is 'test'.
+    """
+    
+    # strip punctuations and replace with space
+    shloka = shloka.translate(string.maketrans(",:", "  ")).strip() 
+    tokens = shloka.split()
+    names = set()
+    result = []
+    relation = set()
+    for token in tokens:
+        root = token
+        names_dict_keys = names_dict.keys()
+        if root in names_dict_keys:
+            names.add(names_dict[root])
+    encountered_name_list = []
+    for name_1 in names:
+        encountered_name_list.append(name_1)
+        for name_2 in [x for x in names if x not in encountered_name_list]:
             if data_type == "train":
-                n_key=n1+"|"+n2
-                n_key_rev = n2+"|"+n1
-                if n_key in name_rel:
-                    rel=name_rel[n_key]
-                elif n_key_rev in name_rel:
-                    rel = name_rel[n_key_rev]
+                key = name_1 + "|" + name_2
+                key_reverse = name_2 + "|" + name_1
+                if key in relation_dict:
+                    relation = relation_dict[key]
+                elif key_reverse in relation_dict:
+                    relation = relation_dict[key_reverse]
                 else:
-                    return res
-                par=[x for x in tokens]
-                res.append([[n1,n2],rel,par]) #first arg is name pair,second in relationship between them,final is the list of all tokes in the sentence
+                    return result
+                features = [x for x in tokens]
+                #first arg is name pair, second in relationship between them, 
+                #final is the list of all tokes in the sentence
+                result.append([[name_1, name_2], relation, features]) 
             else:
-                if n1 in tokens and n2 in tokens:
-                    par = [x for x in tokens]
-                    print n1,n2
+                if name_1 in tokens and name_2 in tokens:
+                    features = [x for x in tokens]
+                    print name_1, name_2
                     for xx in tokens:
                         print xx
                     print "\n"
-                    res.append([[n1,n2],par])
-    return res
+                    result.append([[name_1, name_2], features])
+    return result
 
 if __name__ == "__main__":
-    d={"ram":["ram","shriram","shrihari","hareram"],"sita":["sita","lela","lelavati"]}
-    x=find_list(d)
+    d = {"ram":["ram","shriram","shrihari","hareram"],"sita":["sita","lela",
+        "lelavati"]}
+    x = reverse_dictionary(d)
     print(x)
-    print(find_relation_tuples("ram and sita sitting in a tree",x,{"sita|ram":"married"}))
-# We can segment the input text file with "|" symbol as a list of sentences and each is passed to find_relation_tuples
+    print(find_relation_tuples("ram and sita sitting in a tree", x, 
+        {"sita|ram":"married"}))
+# We can segment the input text file with "|" symbol as a list of sentences 
+# and each is passed to find_relation_tuples
 #Use find list to generate names_all argument mentioned.
 
--- main_function.py
+++ main_function.py
@@ -8,142 +8,163 @@ import codecs as cd
 from collections import Set
 from sklearn.svm import SVC
 
-out=open("features.txt","w")
+out = open("features.txt", "w")
 corpus = []
 classes = []
 relation_dict = {}
-name_syn = {}
-relation_syn = {}
+name_synonyms = {}
+relation_synonyms = {}
 
-def file_extract(file,n_dict,n_rel,data_type):
-    res=[]
-    with open(file,"r") as f:
-        lines=f.readlines()
-        #segments=lines.rstrip().split("|")
-        #print(segments)
-        for s in lines:
-            res=res+p.find_relation_tuples(s.rstrip(),n_dict,n_rel,data_type)
-    return res
+def extract_features(input_file, name_dict, data_type):
+    """Function to extract features from the shlokas in a file. Extracts only the 
+    shlokas that have a name pair in them. 
+
+    Parameters
+    ----------
+    input_file : The file name of the input file from which the features have 
+                to be extracted.
+    name_dict : The dictionary that contains the various synonyms of a given
+                name. The format is {synonym_1: name, synonym_2: name, ...}. 
+    data_type: A string parameter that specifies whether the data in the file is for 
+            for training or testing. 
+    
+    Returns
+    -------
+    A list of the format [name_pair, relationship, list of features]
+    """
+
+    result = []
+    with open(input_file, "r") as f:
+        lines = f.readlines()
+        for shloka in lines:
+            result = result + p.find_relation_tuples(shloka.rstrip(), name_dict,
+                     relation_dict, data_type)
+    return result
 
 def extract_relationships():
-    global relation_syn, relation_dict, name_syn
+    """Function that extracts the relationships and also synonyms for names and 
+    relationships from the files Relaion.txt and RelationshipSynonyms.txt. 
+    Populates the global variables relation_synonyms, relation_dict and 
+    name_synonyms.
+    """
+
+    global relation_synonyms, relation_dict, name_synonyms
+
     with open('Relation.txt') as fd:
         for line in fd:
             tokens = [x.rstrip() for x in line.split('\t')]
             relation_dict[tokens[0] + '|' + tokens[2]] = tokens[1]
-            name_syn[tokens[0]] = [tokens[0]]
-            name_syn[tokens[2]] = [tokens[2]] #to do
+            name_synonyms[tokens[0]] = [tokens[0]]
+            name_synonyms[tokens[2]] = [tokens[2]] #to do
     with open('RelationshipSynonyms.txt') as fd:
         for line in fd:
             tokens = [x.rstrip() for x in line.split("\t")]
-            relation_syn[tokens[0]] = tokens[1:]
+            relation_synonyms[tokens[0]] = tokens[1:]
 
-def vectorize(training_data,test_data):
+def vectorize(training_data, test_data):
+    """Function that creates vectors from the training and test data for the SVM,
+    trains the SVM and makes predictions on the test data.
+    Uses the svm module from scikit. 
+
+    Parameters
+    ----------
+    training_data : A list of lists of the format 
+                    [named_pair, relationship, list of features].
+    test_data : A list of lists of the format [named_pair, list of features].
+    """
+
     global corpus, classes
-    test_corpus=[]
-    tr=relation_dict
-    tr_k=tr.keys()
+
+    test_corpus = []
     for data in training_data:
         named_pair = data[0]
         rel_class = data[1]
         tokens = data[2]
-        if rel_class=="पुत्र":
+        if rel_class == "पुत्र":
             classes.append(rel_class)
         else:
             classes.append("NR")
         corpus.append(' '.join(tokens))
-        #corpus.append(' '.join(tokens))
-    vectorizer = TfidfVectorizer(min_df=0,sublinear_tf=True,use_idf=True)
+    vectorizer = TfidfVectorizer(min_df=0, sublinear_tf=True, use_idf=True)
     X = vectorizer.fit_transform(corpus)
     print(X.toarray())
-    #print X.toarray().shape
     svm = SVC(C=10, gamma=0.0, kernel='linear')
     svm.fit(X, classes)
-    r_s=relation_syn
-    relationship_reverse_dict = p.find_list(relation_syn)
-    g=relationship_reverse_dict.keys()
+    relationship_reverse_dict = p.reverse_dictionary(relation_synonyms)
     for data in test_data:
         named_pair = data[0]
         tokens = data[2]
         test_corpus.append(' '.join(tokens))
         dict_key = named_pair[0] + '|' + named_pair[1]
-        #dict_key=dict_key.decode("utf-8")
-        #dict_key_r=dict_key_r.decode("utf-8")
-        st=""
-        #if dict_key in relation_dict:
-        #    st=relation_dict[dict_key]
-         #   rv=relation_dict[dict_key_r]
-        #print "break"+"\n"
-        #if st!="" and st in tokens:
-        #    print named_pair,st
-
-        #for word in tokens:
-        #    if word in relationship_reverse_dict and (relationship_reverse_dict[word] == st or relationship_reverse_dict[word] == rv):
-         #       print named_pair[0],named_pair[1],st
-          #      break
-
-    #for data in test_data:
-    #    named_pair = data[0]
-    #    tokens = data[1]
-    #    test_corpus.append(' '.join(tokens))
         dict_key_r = named_pair[1] + '|' + named_pair[0]
-        #if relation_dict[dict_key] in tokens or relation_dict[dict_key_r] in tokens:
-         #   print named_pair, relation_dict[dict_key]
-#    test_corpus = open('test','r').readline().rstrip()
-#    t = []
-#    t.append(test_corpus)
-        
+       
     Xtest = vectorizer.transform(test_corpus)
-    pred = svm.predict(Xtest)
-    print "break"+"\n"
-    corr1=0
-    corr2=0
-    total=0
-    for i in range(len(pred)):
-        if pred[i]==test_data[i][1]:
-            corr1+=1
-        elif pred[i]=="NR" and test_data[i][1]!="पुत्र":
-            corr2+=1
-        total+=1
-        print test_data[i][0][0],test_data[i][0][1],pred[i],test_data[i][1]
-    print float(corr1+corr2)/total#,corr1,corr2,total
+    prediction = svm.predict(Xtest)
+    corr1 = 0
+    corr2 = 0
+    total = 0
+    for i in range(len(prediction)):
+        if prediction[i] == test_data[i][1]:
+            corr1 += 1
+        elif prediction[i] == "NR" and test_data[i][1] != "पुत्र":
+            corr2 += 1
+        total += 1
+        print (test_data[i][0][0], test_data[i][0][1], prediction[i], 
+            test_data[i][1])
+    print float(corr1+corr2)/total#, corr1, corr2, total
 
-def split_data(data,train,test):
-    train_res=[]
-    test_res=[]
+def split_data(data, train, test):
+    """Function to split data into two parts one for training and the other for 
+    testing based on the named pairs contained in the data. 
+    The SVM is trained on a few name pairs and then tested upon name pairs with
+    erlationships similar to the ones in training. 
+
+    Parameters
+    ----------
+    data : A list of list containing named pairs and the corresponding features.
+    train : A list of name pairs that the SVM should be trained upon.
+    test : A list of name pairs that the SVM should be tested upon.  
+    
+    Returns
+    -------
+    Two lists, one containing the data for training and the other containing data 
+    for testing.
+    """
+
+    train_result = []
+    test_result = []
     for d in data:
-        if [d[0][0],d[0][1]] in train:
-            train_res.append(d)
-        elif[d[0][0],d[0][1]] in test:
-            test_res.append(d)
-    return train_res,test_res
+        if [d[0][0], d[0][1]] in train:
+            train_result.append(d)
+        elif[d[0][0], d[0][1]] in test:
+            test_result.append(d)
+    return train_result, test_result
 
 if __name__=="__main__":
-#    d={'राम':['राम'], 'सीता':['सीता']}
-#    r={'राम|सीता':'पतिः'}
-    train_l=[["भरत","कैकेयी"],["कैकेयी","भरत"],["राम","भरत"],["भरत","राम"]]
-    test_l=[["राम","कौसल्या"],["कौसल्या","राम"],["राम","सीता"],["सीता","राम"]]
+    train_l = [["भरत","कैकेयी"],["कैकेयी","भरत"],["राम","भरत"],["भरत","राम"]]
+    test_l = [["राम","कौसल्या"],["कौसल्या","राम"],["राम","सीता"],["सीता","राम"]]
     extract_relationships()
-    name_dict=p.find_list(name_syn)
-    data=[]
-    files=glob.glob("data/corpus/processed/*/*")
+    name_dict = p.reverse_dictionary(name_synonyms)
+    data = []
+    files = glob.glob("data/corpus/processed/*/*")
     for f in files:
         if os.path.isfile(f):
-            data=data+file_extract(f,name_dict,relation_dict, "train")
-    files=glob.glob("data/corpus/processed/test_data/*")
+            data = data + extract_features(f, name_dict, "train")
+    files = glob.glob("data/corpus/processed/test_data/*")
     for t in data:
-        out.write(t[0][0]+":"+t[0][1]+":"+t[1]+":")
+        out.write(t[0][0] + ":" + t[0][1] + ":" + t[1] + ":")
         for d in t[2]:
-            out.write(d+":")
+            out.write(d + ":")
         out.write("\n")
     out.close()
-    out=cd.open("features.txt","r","UTF-8")
-    lines=out.readlines()
-    data=[]
+    out = cd.open("features.txt","r","UTF-8")
+    lines = out.readlines()
+    data = []
     for l in lines:
-        l=l.encode("UTF-8")
-        x=l.split(":")
-        data.append([[x[0],x[1]],x[2],x[3:]])
-    train_data,test_data=split_data(data,train_l,test_l)
-    vectorize(train_data,test_data)
-#train_data is a list of training data with each element having [[name1,name2],relationship,[<list of words in sentence>]]
+        l = l.encode("UTF-8")
+        x = l.split(":")
+        data.append([[x[0], x[1]], x[2], x[3:]])
+    train_data,test_data = split_data(data, train_l, test_l)
+    vectorize(train_data, test_data)
+# train_data is a list of training data with each element having 
+# [[name1,name2],relationship,[<list of words in sentence>]]
